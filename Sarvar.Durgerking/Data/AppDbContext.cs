c2c321498c7314afa2848c74e1547b4469a00833
sarvar-swe <s.azodov.software.engineer@gmail.com>
2023-07-25 14:57:38 +0500

    User-Entity

--- Sarvar.Durgerking/Data/AppDbContext.cs
+++ Sarvar.Durgerking/Data/AppDbContext.cs
@@ -12,7 +12,34 @@ public class AppDbContext : DbContext, IAppDbContext
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        modelBuilder.Entity<User>()
+            .HasKey(a => a.Id);
         
+        modelBuilder.Entity<User>()
+            .Property(a => a.Fullname)
+            .HasMaxLength(100)
+            .IsRequired();
+        
+        modelBuilder.Entity<User>()
+            .Property(a => a.Username)
+            .HasMaxLength(50);
+        
+        modelBuilder.Entity<User>()
+            .Property(a => a.Language)
+            .HasMaxLength(10);
+        
+        modelBuilder.Entity<User>()
+            .Property(a => a.Phone)
+            .HasMaxLength(20);
+        
+        modelBuilder.Entity<User>()
+            .Property(a => a.CreatedAt)
+            .HasDefaultValue(new DateTime(2023, 7, 10, 11, 29, 16, 314, DateTimeKind.Utc).AddTicks(6142));
+        
+        modelBuilder.Entity<User>()
+            .Property(a => a.ModifiedAt)
+            .HasDefaultValue(new DateTime(2023, 7, 10, 11, 29, 16, 314, DateTimeKind.Utc).AddTicks(6142));
+
         base.OnModelCreating(modelBuilder);
     }
 
--- Sarvar.Durgerking/Program.cs
+++ Sarvar.Durgerking/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+using Sarvar.Durgerking.Data;
 using Sarvar.Durgerking.Services;
 using Telegram.Bot;
 using Telegram.Bot.Polling;
@@ -8,6 +10,8 @@ builder.Services.AddTransient<IUpdateHandler, UpdateHandler>();
 builder.Services.AddHostedService<BotBackgroundService>();
 builder.Services.AddSingleton<ITelegramBotClient, TelegramBotClient>(
     bot => new TelegramBotClient(builder.Configuration.GetValue("BotApiKey", string.Empty)));
+builder.Services.AddDbContext<IAppDbContext, AppDbContext>(
+    options => options.UseNpgsql(builder.Configuration.GetConnectionString("PostgresConnection")));
 
 var app = builder.Build();
 
