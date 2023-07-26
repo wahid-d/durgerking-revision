007d779e21ee2c2c387f852097ed1e1d1c5c074e
Sarvar Azodov <134887664+sarvar-swe@users.noreply.github.com>
2023-07-26 12:56:59 +0500

    Bot Setup (#5)

--- Sarvar.Durgerking/Program.cs
+++ Sarvar.Durgerking/Program.cs
@@ -1,4 +1,13 @@
+using Sarvar.Durgerking.Services;
+using Telegram.Bot;
+using Telegram.Bot.Polling;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddTransient<IUpdateHandler, UpdateHandler>();
+builder.Services.AddHostedService<BotBackgroundService>();
+builder.Services.AddSingleton<ITelegramBotClient, TelegramBotClient>(
+    bot => new TelegramBotClient(builder.Configuration.GetValue("BotApiKey", string.Empty)));
 
 var app = builder.Build();
 
