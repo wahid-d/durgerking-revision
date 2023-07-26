8fd72f05a40b676ea0431d543acb2b2cac6cf6e6
Sirojiddin <Jstivee@gmail.com>
2023-07-27 02:41:33 +0300

    Updatehandler and BotbackroundService

--- Sirojiddin.Durgerking/Program.cs
+++ Sirojiddin.Durgerking/Program.cs
@@ -1,4 +1,12 @@
-var builder = WebApplication.CreateBuilder(args);
+using Sirojiddin.Durgerking.Services; 
+using Telegram.Bot; 
+using Telegram.Bot.Polling; 
+ 
+var builder = WebApplication.CreateBuilder(args); 
+builder.Services.AddTransient<IUpdateHandler, UpdateHandler>(); 
+builder.Services.AddHostedService<BotBackroundService>(); 
+builder.Services.AddSingleton<ITelegramBotClient, TelegramBotClient>( 
+    p => new TelegramBotClient(builder.Configuration.GetValue("BotApiKey",string.Empty)));
 
 var app = builder.Build();
 
